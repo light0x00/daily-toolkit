fab54ff3a310db8b8798b9c57dbf551a00dac3f2
light <light0x00@163.com>
2023-06-05 14:26:58 +0800

    config

--- packages/ts-demo/src/index.ts
+++ packages/ts-demo/src/index.ts
@@ -1,1 +1,38 @@
-console.log("Hello TS")
+/*
+   next-tick
+   ┌───────────────────────────┐
+┌─>│           timers          │
+│  └─────────────┬─────────────┘
+│  ┌─────────────┴─────────────┐
+│  │     pending callbacks     │
+│  └─────────────┬─────────────┘
+│  ┌─────────────┴─────────────┐
+│  │       idle, prepare       │
+│  └─────────────┬─────────────┘      ┌───────────────┐
+│  ┌─────────────┴─────────────┐      │   incoming:   │
+│  │           poll            │<─────┤  connections, │
+│  └─────────────┬─────────────┘      │   data, etc.  │
+│  ┌─────────────┴─────────────┐      └───────────────┘
+│  │           check           │
+│  └─────────────┬─────────────┘
+│  ┌─────────────┴─────────────┐
+└──┤      close callbacks      │
+   └───────────────────────────┘
+ */
+const baz = () => console.log('baz');
+const foo = () => console.log('foo');
+const zoo = () => console.log('zoo');
+const start = () => {
+  console.log('start2');
+  setImmediate(baz);	// 被放入 check 队列, check phrase
+  new Promise((resolve, reject) => {
+    resolve('bar');
+  }).then((resolve) => {	// 被放入 pending callbacks
+    console.log(resolve);
+    process.nextTick(zoo);	// 被放入 next tick 队列, 在下一轮循环的开始执行
+  });
+  process.nextTick(foo); // 被放入 next tick 队列, 在下一轮循环的开始执行
+};
+start();
+
+// start foo bar zoo baz
